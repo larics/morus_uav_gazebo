2f487d4cc3083d429e1896b18d5968350bfe925a
Lovro Markovic <lovro.markovic@fer.hr>
2019-03-12 16:19:26 +0100

    Add logitech config, add position constraints

--- morus_joy/src/morus_joy_command_node.py
+++ morus_joy/src/morus_joy_command_node.py
@@ -5,27 +5,23 @@ from geometry_msgs.msg import Vector3
 from geometry_msgs.msg import PointStamped
 from sensor_msgs.msg import Joy
 
+def saturation(value, min, max):
+    if (value < min):
+        return min  
+    elif (value > max):
+        return max
+    else:
+        return value
 
 class Commander():
     #carrot following controller for z axis requires knowing the position of the UAV 
-    UAV_pose = PointStamped()
-    # Subscribe to teleop msgs
-    def cmd_vel_callback(self,data):
-        cmd_roll_pitch_yaw = Vector3()
-        cmd_roll_pitch_yaw.x=data.linear.x
-        cmd_roll_pitch_yaw.y=data.linear.y
-        cmd_roll_pitch_yaw.z=data.angular.z
-        self.pub.publish(cmd_roll_pitch_yaw)
-        cmd_position = Vector3()
-        cmd_position.z = self.UAV_pose.point.z+10*data.linear.z #5ms for 20Hz
-        self.pub2.publish(cmd_position)
     
-    def position_callback(self,data):
-        self.UAV_pose = data
-    
-    # Must have __init__(self) function for a class
+    MIN_HEIGHT = 0.7
+    MAX_HEIGHT = 2.5
+
     def __init__(self):
         # Create a publisher for roll pitch yaw cmnds
+        self.UAV_pose = PointStamped()
         self.pub = rospy.Publisher('morus/euler_ref', Vector3, queue_size=1)
         self.pub2 = rospy.Publisher('morus/pos_ref', Vector3, queue_size=1)
         # Initialize message variables.
@@ -37,9 +33,24 @@ class Commander():
         # Main while loop.
         while not rospy.is_shutdown():
             rate.sleep()
+
+    # Subscribe to teleop msgs
+    def cmd_vel_callback(self,data):
         
+        cmd_roll_pitch_yaw = Vector3()
+        cmd_roll_pitch_yaw.x=data.linear.x
+        cmd_roll_pitch_yaw.y=data.linear.y
+        cmd_roll_pitch_yaw.z=data.angular.z
+        self.pub.publish(cmd_roll_pitch_yaw)
 
-
+        cmd_position = Vector3()
+        cmd_position.z = self.UAV_pose.point.z+10*data.linear.z #5ms for 20Hz
+        cmd_position.z = saturation(cmd_position.z, Commander.MIN_HEIGHT, Commander.MAX_HEIGHT)
+        self.pub2.publish(cmd_position)
+    
+    def position_callback(self,data):
+        self.UAV_pose = data
+    
 
 if __name__ == '__main__':
     # Initialize the node and name it.
