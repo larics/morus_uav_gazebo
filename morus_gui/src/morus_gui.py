ab77bacafb3d420119defcf55b79f19b44c73111
Lovro Markovic <lovro.markovic@fer.hr>
2019-03-14 11:21:43 +0100

    Add documentation and resource path softcoding

--- morus_gui/src/morus_gui.py
+++ morus_gui/src/morus_gui.py
@@ -4,42 +4,63 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 import sys
+import rospkg 
 
 from run_gazebo import RunGazeboSimulator
+
 
 class MorusGUI(QtWidgets.QWidget):
     
     INFO_TEXT_INIT = "Please register before playing"
     DEFAULT_BTN_WIDTH = 200
+    LAUNCH_FILE = "/launch/morus_multirotor_height_attitude_ctl.launch"
+    PICTURE_FILE = "/resources/MORUS.png"
 
     def __init__(self):
         super(self.__class__, self).__init__()
         
         self.setupUI()
+        self.setupCallbacks()
+        
+    def setupCallbacks(self):
+        """
+        Setup all needed callbacks for GUI objects.
+        """
 
-        self.start_button.clicked.connect(self.btn1_cb)
-        self.stop_button.clicked.connect(self.btn2_cb)
+        self.start_button.clicked.connect(self.start_btb_callback)
+        self.stop_button.clicked.connect(self.stop_btn_callback)
 
     def setupUI(self):
+        """
+        Setup all visual GUI-related objects with no functionality
+        """
+
+        # Create all buttons
         self.register_button = QPushButton("New User")
         self.start_button = QPushButton("Start")
-        self.start_button.setEnabled(False)
+        #self.start_button.setEnabled(False)
         self.stop_button = QPushButton("Stop")
         self.stop_button.setEnabled(False)
         
+        # Create a visual label
+        rospack = rospkg.RosPack()
+        path = rospack.get_path("morus_gui")
+        pic_path = path + MorusGUI.PICTURE_FILE
+         
         logo_label = QLabel()
-        pixmap = QPixmap('../resources/MORUS.png')
+        pixmap = QPixmap(pic_path)
         logo_label.setPixmap(pixmap)
         logo_label.setAlignment(Qt.AlignCenter)
 
+        # Create information label
         self.info_label = QLabel()
         self.info_label.setAlignment(Qt.AlignCenter)
         newfont = QtGui.QFont("Aerial", 18, QtGui.QFont.Bold) 
         self.info_label.setFont(newfont)
         self.info_label.setText(MorusGUI.INFO_TEXT_INIT)
         
+        # Setup vertical layout
         layout = QVBoxLayout()
-        layout.setAlignment(Qt.AlignCenter)
         layout.addWidget(logo_label)
         layout.addWidget(self.info_label)
         layout.addWidget(self.register_button)
@@ -51,14 +72,30 @@ class MorusGUI(QtWidgets.QWidget):
         self.setWindowTitle("Drone Days 2019")
         self.show()
 
-    def btn1_cb(self):
-        self.workThread = RunGazeboSimulator()
+    def start_btb_callback(self):
+        """
+        Start button callback.rospack = rospkg.RosPack()
+        path = rospack.get_path("morus_gazebo")
+        launch_path = path + MorusGUI.LAUNCH_FILE
+
+        Start the Gazebo simulation.
+        """
+
+        rospack = rospkg.RosPack()
+        path = rospack.get_path("morus_gazebo")
+        launch_path = path + MorusGUI.LAUNCH_FILE
+
+        self.workThread = RunGazeboSimulator(launch_path)
         self.workThread.start()
         self.start_button.setEnabled(False)
         self.stop_button.setEnabled(True)
 
-    def btn2_cb(self):
-        print("Button2 clicked")
+    def stop_btn_callback(self):
+        """
+        Stop button callback.
+        Stop the Gazebo simulation.
+        """
+
         self.workThread.stop_simlation()
         self.start_button.setEnabled(True)
         self.stop_button.setEnabled(False)
--- morus_gui/src/run_gazebo.py
+++ morus_gui/src/run_gazebo.py
@@ -11,18 +11,28 @@ import roslaunch
 import rospy
 
 class RunGazeboSimulator(QThread):
-    def __init__(self):
+    """
+    Class for initializing Gazebo simulator.
+    Uses roslaunch API to execute a roslaunch file inside a Process.
+    """
+
+    def __init__(self, path):
         QThread.__init__(self)
 
+        # Initialize roslaunch object
         uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
         roslaunch.configure_logging(uuid)
         self.launch_sim = roslaunch.parent.ROSLaunchParent(uuid, 
-        ["/home/lmark/Documents/catkin_ws/src/morus_uav_gazebo/morus_gazebo/launch/morus_multirotor_height_attitude_ctl.launch"])
+        [path])
         
     def __del__(self):
         self.wait()
 
     def start_simulation(self):
+        """
+        Start simulation using the roslaunch API.
+        """
+
         print("Inside process: Starting Simulation")
         self.launch_sim.start()
         
@@ -36,9 +46,18 @@ class RunGazeboSimulator(QThread):
         self.launch_sim.shutdown()
 
     def stop_simlation(self):
+        """
+        Stop and joint the Process.
+        """
+
         self.sim_process.terminate()
         self.sim_process.join()
 
     def run(self):
+        """
+        Create a new Process object, append start_simulation handle
+        and start it.
+        """
+
         self.sim_process = Process(target=self.start_simulation)
         self.sim_process.start()
