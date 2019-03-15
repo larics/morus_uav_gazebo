855c2b803cad9d8a63fd57f4c40c149bfd7415ac
Lovro Markovic <lovro.markovic@fer.hr>
2019-03-15 17:33:49 +0100

    Add record and quit buttons

--- morus_gui/src/game_dialog.py
+++ morus_gui/src/game_dialog.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import *
 import rospy
 from std_msgs.msg import Int8, Float64
 
-from loop_thread import LoopMonitor
+from loop_monitor import LoopMonitor
 
 class LoopDialog(QDialog):
     """
@@ -21,26 +21,71 @@ class LoopDialog(QDialog):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUI()
+        self.setupCallbacks()
         
+    def setupCallbacks(self):
+        self.p_button.clicked.connect(self.run_loop)
+        self.record_button.clicked.connect(self.record_btn_callback)
+        self.quit_button.clicked.connect(self.quit_btn_callback)
+
     def setupUI(self):
         self.info_label = QLabel("Press play when ready")
         newfont = QtGui.QFont("Aerial", 15, QtGui.QFont.Bold) 
         self.info_label.setFont(newfont)
+        self.info_label.setAlignment(Qt.AlignCenter)
 
+        labelfont = QtGui.QFont("Aerial", 13, QtGui.QFont.Bold)
         self.time_label = QLabel()
         self.dist_label = QLabel()
+
+        self.time_label.setFont(labelfont)
+        self.dist_label.setFont(labelfont)
+
+        self.time_label.setFixedWidth(100)
+        self.dist_label.setFixedWidth(100)
+
+        t_label = QLabel("Elapsed time: ")
+        t_label.setFont(labelfont)
+        d_label = QLabel("Distance to target: ")
+        d_label.setFont(labelfont)
+        
+        t_layout = QHBoxLayout()
+        t_layout.addWidget(t_label)
+        t_layout.addWidget(self.time_label)
+        t_layout.setSpacing(50)
+
+        d_layout = QHBoxLayout()
+        d_layout.addWidget(d_label)
+        d_layout.addWidget(self.dist_label)
+        d_layout.setSpacing(50)
+
+        btn_layout = QHBoxLayout()
+        self.record_button = QPushButton("Save Score")
+        self.quit_button = QPushButton("Quit")
+        self.record_button.setEnabled(False)
+        self.quit_button.setEnabled(False)
+        self.record_button.setVisible(False)
+        self.quit_button.setVisible(False)
+        btn_layout.addWidget(self.record_button)
+        btn_layout.addWidget(self.quit_button)
+
         self.p_button = QPushButton("Play")
-        self.p_button.clicked.connect(self.run_loop)
 
         v_layout = QVBoxLayout()
         v_layout.addWidget(self.info_label)
-        v_layout.addWidget(self.time_label)
-        v_layout.addWidget(self.dist_label)
+        v_layout.addLayout(t_layout)
+        v_layout.addLayout(d_layout)
         v_layout.addWidget(self.p_button)
+        v_layout.addLayout(btn_layout)
 
         self.setLayout(v_layout)
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle("Game Loop")
+        # enable custom window hint
+        self.setWindowFlags(self.windowFlags() | QtCore.Qt.CustomizeWindowHint)
+
+        # disable (but not hide) close button
+        self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowCloseButtonHint)
         self.show()
 
     def run_loop(self):
@@ -54,18 +99,30 @@ class LoopDialog(QDialog):
         self.monitor.time_signal.connect(self.updateTime)
         self.monitor.start_node()     
 
+    def record_btn_callback(self):
+        self.monitor.stop_node()
+        self.accept()
+
+    def quit_btn_callback(self):
+        self.monitor.stop_node()
+        self.close()
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key == Qt.Key_Escape:
+            # Don't close on escape
+            pass
+
     @pyqtSlot(float)
     def updateDistance(self, arg1):
         print("LoopDialog: Update distance")
-        self.dist_label.setText(str(arg1))
-
+        self.dist_label.setText("{0:.2f} m".format(arg1))
         self.refresh_ui()
 
     @pyqtSlot(float)
     def updateTime(self, arg1):
         print("LoopDialog: Update time")
-        self.time_label.setText(str(arg1))
-        
+        self.time_label.setText("{0:.2f} s".format(arg1))
         self.refresh_ui()
 
     @pyqtSlot(int)
@@ -79,10 +136,16 @@ class LoopDialog(QDialog):
 
         elif arg1 == LoopMonitor.FINISHED:
             self.info_label.setText(LoopDialog.FINISHED_MSG)
+            self.record_button.setEnabled(True)
+            self.record_button.setVisible(True)
+
+            self.quit_button.setEnabled(True)
+            self.quit_button.setVisible(True)
 
         self.refresh_ui()
 
     def refresh_ui(self):
         self.update()
         self.repaint()
+        self.adjustSize()
     
