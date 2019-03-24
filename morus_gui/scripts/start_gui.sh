d838c7ecbf2c7c7c1b5543bef6ea0c4c42bda00d
Lovro Markovic <lovro.markovic@fer.hr>
2019-03-24 15:23:51 +0100

    Add general start script

--- morus_gui/scripts/start_gui.sh
+++ morus_gui/scripts/start_gui.sh
@@ -1,6 +1,10 @@
+#!/bin/zsh
+
+source ~/.zshrc
 killall -9 roscore
 killall -9 rosmaster
 killall gzclient
 killall gzserver
 roscore &
-python3 ../src/gui_main.py
+roscd morus_gui
+python3 src/gui_main.py
